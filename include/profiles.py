b1fedfa7933ceb5654f84c8f57c80fe4395a8613
Gabriel Braga <gabrielbraga@mini-de-gabriel.lan>
2025-04-07 00:45:25 -0300

    remoção de comentários no profiles.py

--- include/profiles.py
+++ include/profiles.py
@@ -2,10 +2,10 @@ from cosmos import ProfileConfig
 from cosmos.profiles import PostgresUserPasswordProfileMapping
 
 airflow_db = ProfileConfig(
-    profile_name="warehouse",  # Nome do profile
-    target_name="dev",         # Nome do target
+    profile_name="warehouse",  
+    target_name="dev",         
     profile_mapping=PostgresUserPasswordProfileMapping(
-        conn_id="container_postgres_db",  # Nome único do conn_id
+        conn_id="container_postgres_db", 
         profile_args={         
             "schema":"airbyte"
         }
